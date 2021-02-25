cbc95c5667405c08fd1e6672e20cc129d174bd90
Johan Bejar <johanbejarc@hotmail.com>
2021-02-25 14:14:46 -0500

    Fixed eslit config bug

--- config.example.js
+++ config.example.js
@@ -1,7 +1,7 @@
-var GITHUB_API_KEY = '';
-var FACEBOOK_API_KEY = '';
+const GITHUB_API_KEY = '';
+const FACEBOOK_API_KEY = '';
 
 module.exports = {
-    GITHUB_API_KEY,
-    FACEBOOK_API_KEY
+  GITHUB_API_KEY,
+  FACEBOOK_API_KEY,
 };
