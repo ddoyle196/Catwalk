8c51a06dde4ab6cd5b4fcbc3cc76dd68488bb902
Johan Bejar <johanbejarc@hotmail.com>
2021-02-25 21:11:20 -0500

    Get questions details from Atelier API

--- client/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ client/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import axios from 'axios';
+
 import Config from '../../../config';
+
+const urlQuestions = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/';
 
 const QuestionsAnswers = class extends React.PureComponent {
   constructor() {
     super();
     this.state = {
-      config: Config.GITHUB_API_KEY,
+      config: '',
     };
+
+    this.getQuestionListById = this.getQuestionListById.bind(this);
+  }
+
+  componentDidMount() {
+    this.getQuestionListById(19378, 1, 1);
+  }
+
+  getQuestionListById(productId, page, count) {
+    axios.get(`${urlQuestions}?product_id=${productId}&page=${page}&count=${count}`, {
+      headers: {
+        Authorization: Config.GITHUB_API_KEY,
+      },
+    })
+      .then((response) => {
+        this.setState({
+          config: JSON.stringify(response.data),
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
+    const { config } = this.state;
     return (
-      <div>{ this.state.config }</div>
+      <div>{ config }</div>
     );
   }
 };
