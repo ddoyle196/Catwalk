d36e41354b020156cce53ae3fbc332e70b34b266
Ross Loney <majoross@gmail.com>
2021-02-27 09:27:45 -0600

    Add GET PRODUCT BY ID to server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const { GITHUB_API_KEY } = require('../config');
 
 const app = express();
 const port = 3000;
@@ -11,6 +13,24 @@ app.get('/test', (req, res) => {
   console.log('GET');
   res.send('Howdy!');
 });
+
+// GET PRODUCT BY ID
+app.get('/products/:id', (req, res) => {
+  const productId = req.params.id;
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${productId}`, {
+    headers: {
+      Authorization: GITHUB_API_KEY,
+    },
+  })
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// GET STYLES FOR A GIVEN PRODUCT
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
