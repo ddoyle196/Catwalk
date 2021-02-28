5f31276f38afd28fd093d74d2c6fdc46271b94a5
Ross Loney <majoross@gmail.com>
2021-02-27 19:16:55 -0600

    Wrap up basic layout for StarRating -- half star symbol still pending

--- client/components/overview/productInfo/StarRating.jsx
+++ client/components/overview/productInfo/StarRating.jsx
@@ -10,12 +10,28 @@ const StarRating = ({ ratings }) => {
     ratingTotal += Number(key) * Number(ratings[key]);
     voteCount += Number(ratings[key]);
   }
-  const starRating = Math.round((ratingTotal / voteCount) * 2) / 2;
+  let blackStars = Math.round((ratingTotal / voteCount) * 2) / 2;
+  let whiteStars = Math.floor(5 - blackStars);
+  let stars = '';
+  while (blackStars > 0.5) {
+    stars += String.fromCharCode(9733); // char code for black rating star
+    blackStars -= 1;
+  }
+
+  if (blackStars > 0) {
+    stars += 'HALF STAR'; // TO DO: FIND HALF STAR SYMBOL TO INSERT HERE
+    // Unicode: U+2BE8 // HTML (decimal) &#11240; // HTML (hex) &#x2be8;
+  }
+
+  while (whiteStars > 0) {
+    stars += String.fromCharCode(9734); // char code for white rating star
+    whiteStars -= 1;
+  }
 
   return (
     <div>
-      STAR RATING:
-      {starRating}
+      {stars}
+      <span>TO DO: A LINK THAT SAYS &quot;Read all reviews&quot;</span>
     </div>
   );
 };
