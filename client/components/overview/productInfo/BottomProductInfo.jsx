142dd33057df6b2aca7a9e4c885b4a51b4aa5bc3
Ross Loney <majoross@gmail.com>
2021-03-01 12:06:01 -0600

    Add social media buttons -- functionality pending

--- client/components/overview/productInfo/BottomProductInfo.jsx
+++ client/components/overview/productInfo/BottomProductInfo.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
+import { InlineIcon } from '@iconify/react';
+import check from '@iconify-icons/mdi/check';
+import facebookIcon from '@iconify-icons/mdi/facebook';
+import twitterIcon from '@iconify-icons/mdi/twitter';
+import pinterestIcon from '@iconify-icons/mdi/pinterest';
+
 import PropTypes from 'prop-types';
 
-const BottomProductInfo = ({ product }) => (
-  <div className="bottom-product-info-container">
-    <div className="bottom-product-info-left-container">
-      {product.slogan && <p>{product.slogan}</p>}
-      {product.description && <p>{product.description}</p>}
+const BottomProductInfo = ({ product }) => {
+  let keyCount = 0;
+  const features = product.features.map((item) => {
+    keyCount += 1;
+    return (
+      <p key={keyCount} className="feature-text-container">
+        <span className="feature-check-icon"><InlineIcon key={keyCount} icon={check} width="1.5em" height="1.5em" /></span>
+        <span className="feature-text">{item.feature}</span>
+      </p>
+    );
+  });
+  return (
+    <div className="bottom-product-info-container">
+      <div className="bottom-product-info-left-container">
+        {product.slogan && <p className="slogan"><strong>{product.slogan}</strong></p>}
+        {product.description && <p className="description">{product.description}</p>}
+      </div>
+      <div className="bottom-product-info-right-container">
+        {features}
+        <div className="social-media-icon-container">
+          <span className="social-media-icon"><InlineIcon icon={facebookIcon} /></span>
+          <span className="social-media-icon"><InlineIcon icon={twitterIcon} /></span>
+          <span className="social-media-icon"><InlineIcon icon={pinterestIcon} /></span>
+        </div>
+      </div>
     </div>
-    <div className="bottom-product-info-right-container">
-      <p>{product.features[0].feature}</p>
-      <p>{product.features[1].feature}</p>
-      <p>{product.features[2].feature}</p>
-    </div>
-  </div>
-);
+  );
+};
 
 BottomProductInfo.propTypes = {
   product: PropTypes.shape({
