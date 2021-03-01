2c4001b470cafa4600df2a496ef13fcf68f76bb9
Ross Loney <majoross@gmail.com>
2021-03-01 10:43:19 -0600

    Format product category, title and price

--- client/components/overview/productInfo/SideProductInfo.jsx
+++ client/components/overview/productInfo/SideProductInfo.jsx
@@ -2,15 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import StarRating from './StarRating';
 
-const SideProductInfo = ({ product, ratings }) => (
-  <div>
-    <StarRating ratings={ratings} />
-    <h2>SIDE PRODUCT INFO COMPONENT</h2>
-    {product.category && <p>{product.category}</p>}
-    {product.name && <p>{product.name}</p>}
-    {product.default_price && <p>{product.default_price}</p>}
-  </div>
-);
+const SideProductInfo = ({ product, ratings }) => {
+  let formattedPrice = '';
+  const removeZerosFromPrice = (price) => {
+    const slicedEnd = price.slice(-2);
+    if (slicedEnd === '00') {
+      formattedPrice = product.default_price.slice(0, -3);
+    } else {
+      formattedPrice = product.default_price;
+    }
+  };
+  removeZerosFromPrice(product.default_price);
+  return (
+    <div>
+      <StarRating ratings={ratings} />
+      {product.category && <p className="product-category">{product.category.toUpperCase()}</p>}
+      {product.name && <p className="expanded-product-name">{product.name}</p>}
+      {product.default_price && (
+        <p>
+          $
+          {formattedPrice}
+        </p>
+      )}
+    </div>
+  );
+};
 
 SideProductInfo.propTypes = {
   product: PropTypes.shape({
