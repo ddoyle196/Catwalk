cb74bc3f1cb65ca66a3af2c7faba02f13c1f273b
Daniel Doyle <ddoyle196@gmail.com>
2021-03-01 17:56:29 -0600

    Switched from dummy data to hardcoded product ID and axios call https://trello.com/c/jb4VOWrQ Fixed review render to add two each button press.

--- client/components/RandR/Histograms.jsx
+++ client/components/RandR/Histograms.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Histograms = ({ ratings }) => (
-  <div>
-    <div> Rating, stars, bars and scales. </div>
-    <div>{ratings.ratings[2]}</div>
-  </div>
-);
+const Histograms = ({ ratings }) => {
+  if (ratings) {
+    return (
+      <div>
+        <div> Rating, stars, bars and scales. </div>
+        <div>{ratings.ratings[2]}</div>
+      </div>
+    );
+  }
+  return null;
+};
 
 // Histograms.propTypes = {
 //   ratings: PropTypes.shape({ ratings: PropTypes.shape }).isRequired,
