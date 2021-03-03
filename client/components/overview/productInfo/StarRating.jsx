94bbc40f4a0d49b7a14806af6df3c53d11545b1e
Daniel Doyle <ddoyle196@gmail.com>
2021-03-03 17:12:05 -0600

    4.4.1.2 The average rating of the product will be displayed in the format of solid or outlined stars, where the solid stars represent the review score. A total of 5 stars should always appear, and the amount filled in should correspond to the average score. The visual for rating should be representative of up to a quarter of a review point.  For example, if the average is 3.8, this should display as 3¾ solid stars and 1¼ outlined stars.
    4.4.1.3 Additionally, the count of total reviews should be listed.

--- client/components/overview/productInfo/StarRating.jsx
+++ client/components/overview/productInfo/StarRating.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable no-restricted-syntax */
 import React from 'react';
 import { InlineIcon } from '@iconify/react';
-import blackStar from '@iconify-icons/mdi/star';
+
 import whiteStar from '@iconify-icons/mdi/star-outline';
+import Q0Star from '@iconify-icons/fluent/star-20-regular';
+
+import Q1Star from '@iconify-icons/fluent/star-one-quarter-20-filled';
+
 import halfStar from '@iconify-icons/mdi/star-half-full';
+import Q2Star from '@iconify-icons/fluent/star-half-20-filled';
+
+
+import Q3Star from '@iconify-icons/fluent/star-three-quarter-20-filled';
+
+import blackStar from '@iconify-icons/mdi/star';
+import Q4Star from '@iconify-icons/fluent/star-20-filled';
 
 import PropTypes from 'prop-types';
 
@@ -15,27 +26,36 @@ const StarRating = ({ ratings }) => {
     ratingTotal += Number(key) * Number(ratings[key]);
     voteCount += Number(ratings[key]);
   }
-  let blackStars = Math.round((ratingTotal / voteCount) * 2) / 2;
-  let whiteStars = Math.floor(5 - blackStars);
+  // let blackStars = (Math.round((ratingTotal / voteCount) * 4) / 4).toFixed(2);
+  let blackStars = 3.25;
+  let whiteStars = (5 - blackStars);
   const stars = [];
-  while (blackStars > 0.5) {
-    stars.push(<InlineIcon key={blackStars} icon={blackStar} />);
-    blackStars -= 1;
-  }
 
-  if (blackStars > 0) {
-    stars.push(<InlineIcon key={blackStars} icon={halfStar} />);
+  while (blackStars > 0) {
+    console.log(blackStars);
+    if (blackStars > 1) {
+      stars.push(<InlineIcon key={blackStars} icon={Q4Star} />);
+      blackStars -= 1.00;
+    } else if (blackStars >= 0.75) {
+      stars.push(<InlineIcon key={blackStars} icon={Q3Star} />);
+      blackStars -= 0.75;
+    } else if (blackStars >= 0.5) {
+      stars.push(<InlineIcon key={blackStars} icon={Q2Star} />);
+      blackStars -= 0.50;
+    } else if (blackStars >= 0.25) {
+      stars.push(<InlineIcon key={blackStars} icon={Q1Star} />);
+      blackStars -= 0.25;
+    }
   }
-
-  while (whiteStars > 0) {
-    stars.push(<InlineIcon key={whiteStars - 1} icon={whiteStar} />);
+  while (whiteStars > 1) {
+    stars.push(<InlineIcon key={whiteStars - 1} icon={Q0Star} />);
     whiteStars -= 1;
   }
 
   return (
     <div>
       {stars}
-     {/*<span>  LINK: Read all reviews</span>*/}
+      {/*<span>  LINK: Read all reviews</span>*/}
     </div>
   );
 };
