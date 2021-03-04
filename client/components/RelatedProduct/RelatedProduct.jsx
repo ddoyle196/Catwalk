a6d31467a1cbe6dec36f75fcb2ba2490debbee4c
Johan Bejar <johanbejarc@hotmail.com>
2021-03-04 02:22:06 -0500

    Ticket 6.1.1 Created related product component

---
+++ client/components/RelatedProduct/RelatedProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RelatedProduct = class extends React.PureComponent {
+  render() {
+    return (
+      <div>hi</div>
+    );
+  }
+};
+
+export default RelatedProduct;
