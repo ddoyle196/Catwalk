eb74b382a28018c272ae4cfa8a59185a628e7a2e
Johan Bejar <johanbejarc@hotmail.com>
2021-03-08 11:57:28 -0500

    6.1.3 Create Server routes to get products and image thumbnails

--- client/components/RelatedProduct/RelatedProduct.jsx
+++ client/components/RelatedProduct/RelatedProduct.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 import ProductCard from './ProductCard';
+import { GITHUB_API_KEY } from '../../../config';
+
+const pId = 19378;
 
 const RelatedProduct = class extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      relatedProducts: [],
+    };
+    this.getRelatedProducts = this.getRelatedProducts.bind(this);
+  }
+
+  componentDidMount() {
+    this.getRelatedProducts();
+  }
+
+  getRelatedProducts() {
+    const { relatedProducts } = this.state;
+    axios.get(`/related/${pId}`, {
+      headers: {
+        Authorization: GITHUB_API_KEY,
+      },
+    })
+      .then((result) => {
+        console.log(result.data);
+        this.setState({
+          relatedProducts: result.data,
+        });
+      });
+  }
+
   render() {
     return (
       <div className="rp-box">
